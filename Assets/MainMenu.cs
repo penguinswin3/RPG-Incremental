d03f4d75945aa12c2d65f8a80cfedb49bae9522d
penguinswin3 <bradbcarlin@gmail.com>
2020-06-28 21:54:29 -0400

    initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame(){
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+
+	public void OptionsMenu(){
+		//TODO: Switch to options menu 
+		EditorUtility.DisplayDialog("Not yet Implemented", "Options menu in progress", "ok", "ok");
+	}
+}
