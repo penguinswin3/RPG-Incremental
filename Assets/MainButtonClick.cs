a5a3dae42bddf9f913d2511c95b8d1d2d53dd32c
penguinswin3 <bradbcarlin@gmail.com>
2020-06-28 22:01:20 -0400

    adding files

---
+++ Assets/MainButtonClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButtonClick : MonoBehaviour {
+
+	public GameObject textBox;
+
+	public void ClickTheButton(){
+		textBox.SetActive (true);
+	}
+}
